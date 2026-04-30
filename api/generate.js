a5c9a8afaaad793c1f2e6bcb885d7723cd95241a
unknown <telliacatherine@gmail.com>
2026-04-29 22:10:11 -0400

    debug key

--- api/generate.js
+++ api/generate.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  if (req.method !== 'POST') {
+  if (req.method !== 'POST') console.log('Key starts with:', (process.env.ANTHROPIC_API_KEY || 'MISSING').substring(0, 10)); {
     return res.status(405).json({ error: 'Method not allowed' });
   }
 
