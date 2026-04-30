1999b3e0f546f4d2e0dbee8aab0db2e2c4051c9f
unknown <telliacatherine@gmail.com>
2026-04-29 22:13:35 -0400

    fix module exports

--- api/generate.js
+++ api/generate.js
@@ -1,4 +1,4 @@
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   if (req.method !== 'POST') console.log('Key starts with:', (process.env.ANTHROPIC_API_KEY || 'MISSING').substring(0, 10)); {
     return res.status(405).json({ error: 'Method not allowed' });
   }
